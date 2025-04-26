237ee1330d9fcc2cd9874dccba7c6ab1f682b837
Creitin Gameplays <107757785+CreitinGameplays123@users.noreply.github.com>
2025-04-26 11:00:13 -0300

    Update v4.0.py

--- bot/v4.0.py
+++ bot/v4.0.py
@@ -553,7 +553,7 @@ async def handle_message(message):
                         new_chunks = ["..."]
                     if new_chunks:
                         new_chunks[0] = new_chunks[0].replace("Gemini:", "", 1)
-                        new_chunks[0] = new_chunks[0].replace("Language Model#3241", "", 1)
+                        new_chunks[0] = new_chunks[0].replace("Language Model#3241:", "", 1)
                     new_chunks = ["‎ " if chunk == "\n" else chunk for chunk in new_chunks]
                     for i in range(len(new_chunks)):
                         try:
