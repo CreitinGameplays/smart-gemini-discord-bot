0233c25719228feaa31d866073aa2f66e4f57a82
Creitin Gameplays <clton999@outlook.com>
2025-06-02 09:29:27 -0300

    #

--- bot/main.py
+++ bot/main.py
@@ -636,7 +636,7 @@ async def handle_message(message):
         # Process Gemini response
         while True:
             try:
-                chunk = await asyncio.wait_for(asyncio.to_thread(next, response_stream), timeout=60)
+                chunk += await asyncio.wait_for(asyncio.to_thread(next, response_stream), timeout=60)
             except asyncio.TimeoutError:
                 await bot_message.edit(content="<:aw_snap:1379058439963017226> Sorry, the API did not return any data for over 60 seconds. Please try again.")
                 await asyncio.sleep(8)
